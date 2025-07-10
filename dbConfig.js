3559e2c6f0a2e5e26a1a6db70f69f2ea8648a093
Lokeshwari002 <lokeshwari002@gmail.com>
2025-07-10 17:07:04 +0530

    updated files

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,15 @@
 const express=require('express')
 const mysql=require('mysql2')
+require('dotenv').config
 
 
 
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Kaviyazhini@09",
-    database:"email"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
 })
 
 db.connect((err)=>{
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express=require('express');
 const router = require('./routes');
 const app=express();
 const cors=require('cors')
+require('dotenv').config
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors())
 app.use("/api",router)
 
+const PORT=process.env.DB_PORT||5000
 
-app.listen(8006,()=>{
-    console.log("server is listening at port 8006")
+app.listen(PORT,()=>{
+    console.log(`server is listening at port ${PORT}`)
 })
